94d5efe8dff7f01397ae042d285de2597dc547af
Bhagyshree <Bhagyshree@192.168.1.42>
2022-11-14 10:23:10 +0530

    Latest code push 11-11-2022

--- Xshore/src/main/java/Xshore/App.java
+++ Xshore/src/main/java/Xshore/App.java
@@ -18,7 +18,7 @@ public class App {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		System.out.println("Chrome Browser Opened");
-		driver.manage().timeouts().pageLoadTimeout(2, TimeUnit.MINUTES);
+		driver.manage().timeouts().pageLoadTimeout(3, TimeUnit.MINUTES);
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://xshore.com/xshore-1");
@@ -32,6 +32,8 @@ public class App {
 		driver.findElement(By.id("onetrust-accept-btn-handler")).click();
 		System.out.println("click on accept Cookies");
 		Thread.sleep(2000);
+		
+		
 
 	}
 
@@ -54,17 +56,31 @@ public class App {
 				.click();
 		System.out.println("click on SeaTrial Menu");
 		Thread.sleep(2000);
-
+		
+		driver.findElement(By.xpath("//*[@id='html-body']/div[3]/header/div[1]/div[1]/div/div[2]/div/ul/li[5]/a")).click();
+		System.out.println("click on Design & Order");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div/div/div[1]/div[2]/div/a")).click();
+		System.out.println("click on Design & Order Eelex 8000");
+		Thread.sleep(2000);
+		
+		
+	}
+	@Test(priority = 4)
+	public void Technology() throws InterruptedException {
+		
+		driver.findElement(By.xpath("//*[@id='html-body']/div[3]/header/div[1]/div[1]/div/div[2]/div/ul/li[3]/a")).click();
+		System.out.println("click on FAQ");
+		Thread.sleep(2000);
+		
 		driver.findElement(By.xpath("//button[@type='button']//img")).click();
 		System.out.println("click on Menu");
 		Thread.sleep(2000);
 		
-		
-		driver.findElement(By.xpath("//*[@id='html-body']/div[3]/header/div[2]/div[1]/ul/div/li[3]/a")).click();
-		System.out.println("click on Design & Order");
+		driver.findElement(By.xpath("//*[@id='html-body']/div[3]/header/div[2]/div[1]/ul/div/li[5]/a")).click();
+		System.out.println("click on Technology");
 		Thread.sleep(2000);
-		
-		
 	}
 
 }
